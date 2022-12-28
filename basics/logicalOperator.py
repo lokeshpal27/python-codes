a895dadd9b2b520e56d0f1f769279f36b307b2b5
Lokesh Pal <114469389+lokeshpal27@users.noreply.github.com>
2022-12-28 19:10:26 +0530

    Add files via upload
    here is the very basic code of logical operator

---
+++ basics/logicalOperator.py
@@ -0,0 +1,13 @@
+# there are three main types of logical operators that are
+
+
+
+# and , or , not 
+# and will return true only if every value is True
+# or gives true if one of the value is true
+# not is used for only one value it reverses the value if true it will return false and vice versa
+bool1= True
+bool2= False
+print("the value of bool1 and bool2 is",(bool1 and bool2))
+print("the value of bool1 not bool2 is",(bool1 or bool2))
+print("the value of or bool2 is",(not bool2))
